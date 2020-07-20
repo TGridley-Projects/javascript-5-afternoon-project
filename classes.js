237735caf14cc626d2676811734762e62b79b6c9
Trevor <alutiiq@comcast.net>
2020-07-20 15:15:37 -0700

    Done except for return function

--- classes.js
+++ classes.js
@@ -30,7 +30,17 @@
 */
 
 //Code Here
-
+class Employee {
+  constructor( first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -48,6 +58,19 @@
 */
 
 //Code Here
+class Manager extends Employee {
+   constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age)
+    this.reports = []
+    return this
+  }
+  hire(newEmployee){
+    this.reports.push(newEmployee)
+  }
+  fire(index){
+    this.reports.splice(index, 1)
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -72,6 +95,28 @@
 */
 
 //Code Here
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports){
+    super(first_name, last_name, email, age, reports)
+    this.title = 'Not a manager'
+    this.bonus = 0
+  }
+  hire(newEmployee){
+    this.reports.push(newEmployee)
+    const num= this.reports.length
+    if(num > 0 && num <= 3){this.title = 'Barely Manager'
+    } else if(num >= 4 && num <= 10){ this.title = 'Mostly Manager'
+    } else if(num >= 11 && num <= 50){this.title = 'Manager'
+    } else if(num >= 51 && num <+100){this.title = 'Manager Plus'
+    } else if(num >= 101){this.title = 'Bestest Manager'
+    } else {this.title = 'Not a manager'} 
+  }
+  fire(index){
+    this.bonus += 100
+    this.reports.splice(index, 1);
+     
+  }
+}
 
 
 
@@ -99,5 +144,23 @@
 */
 
 //Code Here
-
-
+class Machine{
+  constructor(){
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
+  makeWidgets(num){
+    this.widgets_made_count += num;
+    if(this.widgets_made_count % 50 === 0){this.wear_and_tear_count = this.widgets_made_count/50};
+  }
+  fixMachine(){
+    this.needs_reboot = true;
+  }
+  reboot(){
+    return function(){
+      Machine.wear_and_tear_count -= 10;
+      Machine.needs_reboot = false;
+    }
+  }
+}
